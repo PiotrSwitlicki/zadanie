cc5fa7c73dc0f96610f9464ad3955dcfe23106c2
PiotrSwitlicki <75681208+PiotrSwitlicki@users.noreply.github.com>
2022-01-03 08:25:04 +0100

    Add files via upload

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,12 +27,20 @@ class HomeController extends Controller
     {
         $id = auth()->user()->id;  
         $user = User::with('wallet')->find($id);  
-           
+        //dd($user);   
         $wallet = $user->wallet;
-       
+        //dd($wallet->ballance);
         
 
+        $api_url = 'http://api.nbp.pl/api/exchangerates/rates/A/USD/';
 
+        // Read JSON file
+        $json_data = file_get_contents($api_url);
+
+        // Decode JSON data into PHP array
+        $response_data = json_decode($json_data);
+
+    //    dd($response_data);
 
         return view('home', ['user' => $user, 'wallet' => $wallet]);
     }
